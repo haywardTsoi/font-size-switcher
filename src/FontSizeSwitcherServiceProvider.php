3fc16cfae1111d108723d53c089bc552ed12f083
haywardTsoi <183706412+haywardTsoi@users.noreply.github.com>
2025-08-20 09:12:07 +0000

    Fix styling

--- src/FontSizeSwitcherServiceProvider.php
+++ src/FontSizeSwitcherServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Solutionforest\FontSizeSwitcher\Commands\FontSizeSwitcherCommand;
+use Solutionforest\FontSizeSwitcher\Testing\TestsFontSizeSwitcher;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Solutionforest\FontSizeSwitcher\Commands\FontSizeSwitcherCommand;
-use Solutionforest\FontSizeSwitcher\Testing\TestsFontSizeSwitcher;
 
 class FontSizeSwitcherServiceProvider extends PackageServiceProvider
 {
